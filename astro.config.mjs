391bc6f608ec124183f88dda0ed34a24e11d201f
Leonardo <leonardoalipazagab@gmail.com>
2023-12-24 00:39:13 -0500

    feat: add new step for copying

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [tailwind(), solidJs({
-    include: ['**/solid/*']
-  })],
-  adapter: netlify()
+  integrations: [
+    tailwind(),
+    solidJs({
+      include: ['**/solid/*'],
+    }),
+  ],
+  adapter: netlify(),
 });
