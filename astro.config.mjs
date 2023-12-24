03ec1574441791c874895fbb8b0b1d27a123c7e5
Leonardo <leonardoalipazagab@gmail.com>
2023-12-23 20:37:51 -0500

    chore: use netlify instead of node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
-import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +10,5 @@ export default defineConfig({
   integrations: [tailwind(), solidJs({
     include: ['**/solid/*']
   })],
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: netlify()
 });
